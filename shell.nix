bb198f1f9c75a00a0e39397955a8e3600724bc02
labi-le <1a6i1e@gmail.com>
2025-11-23 01:06:37 +0300

    add shell nix rust toolchain

--- shell.nix
+++ shell.nix
@@ -1,7 +1,25 @@
-{ pkgs ? import <nixpkgs> {} }:
+{
+  pkgs ? import <nixpkgs> { },
+}:
 
 pkgs.mkShell {
   buildInputs = [
-    pkgs.pkgsCross.aarch64-multiplatform.stdenv.cc
+    pkgs.rustup
+    pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc
   ];
+
+  shellHook = ''
+    export RUSTUP_HOME="$PWD/.rustup"
+    export CARGO_HOME="$PWD/.cargo"
+    export PATH="$CARGO_HOME/bin:$PATH"
+
+    if ! rustup toolchain list | grep -q '^stable'; then
+      rustup toolchain install stable
+    fi
+    rustup default stable
+
+    rustup target add aarch64-unknown-linux-musl
+
+    export CARGO_TARGET_AARCH64_UNKNOWN_LINUX_MUSL_LINKER="${pkgs.pkgsCross.aarch64-multiplatform-musl.stdenv.cc.targetPrefix}cc"
+  '';
 }
